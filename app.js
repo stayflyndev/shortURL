6ab11a7c906044f9f0d7a73ab42c7faed791a65f
stayflyndev <aretee64@gmail.com>
2021-12-09 19:36:22 -0500

    short url

---
+++ app.js
@@ -0,0 +1,32 @@
+const https = require('https');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = https.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at https://${hostname}:${port}/`);
+});
+
+const url = 'https://api.shrtco.de/v2/shorten?url=google.com';
+
+
+https.get(url, res => {
+  let data = '';
+  res.on('data', chunk => {
+    data += chunk;
+  });
+  res.on('end', () => {
+    data = JSON.parse(data);
+    console.log(data.result.short_link);
+  })
+}).on('error', err => {
+  console.log(err.message);
+}).end()
+
+
---
+++ design/app.js
@@ -0,0 +1,33 @@
+const https = require('https');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = https.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at https://${hostname}:${port}/`);
+});
+
+const url = 'https://api.shrtco.de/v2/shorten?url={apiUrl}';
+
+
+https.get(url, res => {
+  let data = '';
+  res.on('data', chunk => {
+    data += chunk;
+  });
+  res.on('end', () => {
+    data = JSON.parse(data);
+    console.log(data.result.short_link);
+  })
+}).on('error', err => {
+  console.log(err.message);
+}).end()
+
+
+const value = "google.com"
